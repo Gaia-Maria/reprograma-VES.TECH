6ed6c24158e5966b890ae0b3e218767e3f8a2204
Gaia Maria <100348271+Gaia-Maria@users.noreply.github.com>
2022-07-17 14:22:21 -0300

    :recycle: refactor: code refactoring

---
+++ src/models/institutionsModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const institutionSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId
+    },
+    institution: { type: String, required: true, unique: true },
+    site: { type: String, pattern: true, required: true },
+    email: { type: String, pattern: true, required: true },
+    description: { type: String, required: true, minLength: 0, maxLength: 500 },
+  },
+  { timestamp: true }
+);
+
+const Model = mongoose.model("institution", institutionSchema);
+
+module.exports = Model;
